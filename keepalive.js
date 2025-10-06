49019ff9eed8b877504f2b6a21ee8fbdd5e982a7
pepe tellez <mcbelme92@gmail.com>
2025-10-06 00:52:25 -0600

    fix: se modifica nodefetch nuevamente por fetch nativo

--- keepalive.js
+++ keepalive.js
@@ -1,10 +1,16 @@
-const fetch = require('node-fetch');
-
-
 const url = process.env.RENDER_EXTERNAL_URL || "https://mini-nginx-server.onrender.com";
 
-setInterval(() => {
-  fetch(url)
-    .then(() => console.log(`[KeepAlive] Ping sent at ${new Date().toISOString()}`))
-    .catch(err => console.error("[KeepAlive] Error:", err.message));
-}, 10 * 60 * 1000);
+async function keepAlive() {
+  try {
+    const res = await fetch(url);
+    console.log(`[KeepAlive] Ping ${res.status} at ${new Date().toISOString()}`);
+  } catch (err) {
+    console.error("[KeepAlive] Error:", err.message);
+  }
+}
+
+// Primer ping inmediato
+keepAlive();
+
+// Cada 10 minutos
+setInterval(keepAlive, 10 * 60 * 1000);
