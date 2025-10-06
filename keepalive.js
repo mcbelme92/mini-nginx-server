9079fff83dc80337a7fa10f9857505f77897bc1d
pepe tellez <mcbelme92@gmail.com>
2025-10-05 22:33:33 -0600

    Feat: se agrega configuracion extra para m anterner activo el server en onrender cada 10 minutos  junto con dikcer.sh que es un script y nodejs npm para usar la libreria node-getch

---
+++ keepalive.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+const url = process.env.RENDER_EXTERNAL_URL || "https://mini-nginx-server.onrender.com";
+
+setInterval(() => {
+  fetch(url)
+    .then(() => console.log(`[KeepAlive] Ping sent at ${new Date().toISOString()}`))
+    .catch(err => console.error("[KeepAlive] Error:", err.message));
+}, 10 * 60 * 1000);
